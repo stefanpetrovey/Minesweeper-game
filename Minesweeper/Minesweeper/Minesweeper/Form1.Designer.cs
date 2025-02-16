9adc8b52d1a3be5bff65186228c681aeb3ad268d
stefanpetrovey <stefanpetrow55@gmail.com>
2025-02-16 02:37:03 +0100

    Add files via upload

---
+++ Minesweeper/Minesweeper/Minesweeper/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelShow = new System.Windows.Forms.Panel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.tbFlag = new System.Windows.Forms.TextBox();
+            this.tbScore = new System.Windows.Forms.TextBox();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.btnEasy = new System.Windows.Forms.Button();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panelShow.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelShow
+            // 
+            this.panelShow.Controls.Add(this.progressBar);
+            this.panelShow.Controls.Add(this.tbFlag);
+            this.panelShow.Controls.Add(this.tbScore);
+            this.panelShow.Controls.Add(this.btnHard);
+            this.panelShow.Controls.Add(this.btnEasy);
+            this.panelShow.Location = new System.Drawing.Point(13, 13);
+            this.panelShow.Name = "panelShow";
+            this.panelShow.Size = new System.Drawing.Size(412, 60);
+            this.panelShow.TabIndex = 0;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(227, 33);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(182, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // tbFlag
+            // 
+            this.tbFlag.Location = new System.Drawing.Point(227, 5);
+            this.tbFlag.Name = "tbFlag";
+            this.tbFlag.ReadOnly = true;
+            this.tbFlag.Size = new System.Drawing.Size(76, 22);
+            this.tbFlag.TabIndex = 3;
+            // 
+            // tbScore
+            // 
+            this.tbScore.Location = new System.Drawing.Point(309, 4);
+            this.tbScore.Name = "tbScore";
+            this.tbScore.ReadOnly = true;
+            this.tbScore.Size = new System.Drawing.Size(100, 22);
+            this.tbScore.TabIndex = 2;
+            // 
+            // btnHard
+            // 
+            this.btnHard.Location = new System.Drawing.Point(4, 33);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(107, 23);
+            this.btnHard.TabIndex = 1;
+            this.btnHard.Text = "Hard";
+            this.btnHard.UseVisualStyleBackColor = true;
+            this.btnHard.Click += new System.EventHandler(this.btnHard_Click);
+            // 
+            // btnEasy
+            // 
+            this.btnEasy.Location = new System.Drawing.Point(4, 4);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(107, 23);
+            this.btnEasy.TabIndex = 0;
+            this.btnEasy.Text = "Easy";
+            this.btnEasy.UseVisualStyleBackColor = true;
+            this.btnEasy.Click += new System.EventHandler(this.btnEasy_Click);
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.Location = new System.Drawing.Point(13, 80);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(411, 347);
+            this.panelButtons.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(437, 439);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.panelShow);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Minesweeper";
+            this.panelShow.ResumeLayout(false);
+            this.panelShow.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelShow;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox tbFlag;
+        private System.Windows.Forms.TextBox tbScore;
+        private System.Windows.Forms.Button btnHard;
+        private System.Windows.Forms.Button btnEasy;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Minesweeper/Minesweeper/Minesweeper/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateButtons();
+            GenerateMines();
+            GenerateNumbers();
+        }
+
+        byte[,] Positions = new byte[12, 12];
+        Button[,] ButtonList = new Button[12, 12];
+        Random rnd = new Random();
+        int timeLeft;
+        int time;
+
+        public void GenerateMines()
+        {
+            int bombs = 0;
+            while (bombs < 20)
+            {
+                int x = rnd.Next(0, 12);
+                int y = rnd.Next(0, 12);
+
+                if (Positions[x, y] == 0)
+                {
+                    Positions[x, y] = 10;
+                    bombs++;
+                }
+            }
+        }
+
+        public void GenerateNumbers()
+        {
+            for (int x = 0; x < 12; x++)
+            {
+                for (int y = 0; y < 12; y++)
+                {
+                    if (Positions[x, y] == 10)
+                        continue;
+                    byte bombCounts = 0;
+                    for (int counterX = -1; counterX < 2; counterX++)
+                    {
+                        int checkerX = x + counterX;
+
+                        for (int counterY = -1; counterY < 2; counterY++)
+                        {
+                            int checkerY = y + counterY;
+
+                            if (checkerX == -1 || checkerY == -1 || checkerX > 11 || checkerY > 11)
+                                continue;
+                            if (checkerY == y && checkerX == x)
+                                continue;
+                            if (Positions[checkerX, checkerY] == 10)
+                                bombCounts++;
+                        }
+                    }
+
+                    if (bombCounts == 0)
+                        Positions[x, y] = 20;
+                    else
+                        Positions[x, y] = bombCounts;
+                }
+            }
+        }
+
+        public void GenerateButtons()
+        {
+            int xLoc = 3;
+            int yLoc = 6;
+            for (int x = 0; x < 12; x++)
+            {
+                for (int y = 0; y < 12; y++)
+                {
+                    Button button = new Button();
+                    button.Parent = panelButtons;
+                    button.Location = new Point(xLoc, yLoc);
+                    button.Size = new Size(25, 25);
+                    button.Tag = $"{x},{y}";
+                    button.Click += BtnClick;
+                    button.MouseUp += RightClickBtn;
+                    xLoc += 25;
+                    ButtonList[x, y] = button;
+                }
+                yLoc += 22;
+                xLoc = 3;
+            }
+        }
+
+
+        public void BtnClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int x = Convert.ToInt32(button.Tag.ToString().Split(',').GetValue(0));
+            int y = Convert.ToInt32(button.Tag.ToString().Split(',').GetValue(1));
+            byte value = Positions[x, y];
+
+            if (value == 10)
+            {
+                button.Image = Properties.Resources.bomb;
+                timer1.Stop();
+                MessageBox.Show("Gameover, you lost!");
+                panelButtons.Enabled = false;
+            }
+
+            else if (value == 20)
+            {
+                button.FlatStyle = FlatStyle.Flat;
+                button.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                button.FlatAppearance.BorderSize = 1;
+                button.Enabled = false;
+                OpenAdjacentEmptyTile(button);
+                points++;
+            }
+            else
+            {
+                button.FlatStyle = FlatStyle.Flat;
+                button.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                button.FlatAppearance.MouseDownBackColor = button.BackColor;
+                button.FlatAppearance.MouseOverBackColor = button.BackColor;
+                button.Text = Positions[x, y].ToString();
+                points++;
+            }
+            button.Click -= BtnClick;
+            tbScore.Text = points.ToString();
+
+            time = 5;
+            timeLeft = 0;
+            progressBar.Maximum = 5;
+            progressBar.Value = 0;
+        }
+
+        public void OpenAdjacentEmptyTile(Button button)
+        {
+            int x = Convert.ToInt32(button.Tag.ToString().Split(',').GetValue(0));
+            int y = Convert.ToInt32(button.Tag.ToString().Split(',').GetValue(1));
+
+            List<Button> emptyButtons = new List<Button>();
+
+            for (int counterX = -1; counterX < 2; counterX++)
+            {
+                int checkerX = x + counterX;
+                for (int counterY = -1; counterY < 2; counterY++)
+                {
+                    int checkerY = y + counterY;
+                    if (checkerX == -1 || checkerY == -1 || checkerX > 11 || checkerY > 11)
+                        continue;
+                    if (checkerY == y && checkerX == x)
+                        continue;
+                    Button buttonAdjacent = ButtonList[checkerX, checkerY];
+                    int xAdjacent = Convert.ToInt32(buttonAdjacent.Tag.ToString().Split(',').GetValue(0));
+                    int yAdjacent = Convert.ToInt32(buttonAdjacent.Tag.ToString().Split(',').GetValue(1));
+                    byte value = Positions[xAdjacent, yAdjacent];
+                    if (value == 20)
+                    {
+                        if (buttonAdjacent.FlatStyle != FlatStyle.Flat)
+                        {
+                            buttonAdjacent.FlatStyle = FlatStyle.Flat;
+                            buttonAdjacent.FlatAppearance.BorderSize = 0;
+                            buttonAdjacent.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                            buttonAdjacent.Enabled = false;
+                            emptyButtons.Add(buttonAdjacent);
+                            points++;
+                        }
+                    }
+                    else if (value != 10)
+                    {
+                        buttonAdjacent.PerformClick();
+                    }
+
+                }
+            }
+            foreach (var buttonEmpty in emptyButtons)
+            {
+                OpenAdjacentEmptyTile(buttonEmpty);
+            }
+            tbScore.Text = points.ToString();
+        }
+
+        int flag = 20;
+
+        int points = 0;
+
+        private void RightClickBtn(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Button btn = (Button)sender;
+                if (btn.FlatStyle != FlatStyle.Flat)
+                {
+                    if (btn.Image == null)
+                    {
+                        btn.Click -= BtnClick;
+                        btn.Image = Properties.Resources.flag;
+                        flag--;
+                    }
+                    else
+                    {
+                        btn.Click += BtnClick;
+                        btn.Image = null;
+                        flag++;
+                    }
+                }
+                tbFlag.Text = flag.ToString();
+            }
+        }
+
+        private void btnEasy_Click(object sender, EventArgs e)
+        {
+            points = 0;
+            flag = 30;
+
+            for (int x = 0; x < 12; x++)
+            {
+                for (int y = 0; y < 12; y++)
+                {
+                    ButtonList[x, y].Dispose();
+                }
+            }
+            panelButtons.Controls.Clear();
+            ButtonList = new Button[12, 12];
+            Positions = new byte[12, 12];
+            panelButtons.Enabled = true;
+            tbScore.Text = null;
+
+            tbFlag.Text = null;
+            time = 5;
+            timeLeft = 0;
+            progressBar.Value = 0;
+            timer1.Stop();
+
+            GenerateMines();
+            GenerateNumbers();
+            GenerateButtons();
+        }
+
+        private void btnHard_Click(object sender, EventArgs e)
+        {
+            points = 0;
+            flag = 30;
+
+            for (int x = 0; x < 12; x++)
+            {
+                for (int y = 0; y < 12; y++)
+                {
+                    ButtonList[x, y].Dispose();
+                }
+            }
+            panelButtons.Controls.Clear();
+            ButtonList = new Button[12, 12];
+            Positions = new byte[12, 12];
+            panelButtons.Enabled = true;
+            tbScore.Text = null;
+
+            tbFlag.Text = null;
+            time = 5;
+            timeLeft = 0;
+
+            timer1.Start();
+
+            GenerateMines();
+            GenerateNumbers();
+            GenerateButtons();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar.Value == progressBar.Maximum)
+            {
+                tbFlag.Text = null;
+                timer1.Stop();
+                MessageBox.Show("Gameover, you lost!");
+                panelButtons.Enabled = false;
+            }
+            if (timeLeft != progressBar.Maximum)
+            {
+                tbFlag.Text = time.ToString();
+                timeLeft += 1;
+                time -= 1;
+            }
+            progressBar.Value = timeLeft;
+        }
+    }
+}
